038bb676992b2976e2e899cb0d182be64b2f04c1
Pedro Klepa <pedro.klepa@gmail.com>
2020-12-14 12:53:53 +0000

    Add basic rendering of index and form pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var dateFns = require("date-fns");
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: dateFns.format(new Date(), "dd/MMM/y '-' hh:mm:ss"),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: dateFns.format(new Date(), "dd/MMM/y '-' hh:mm:ss"),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messaging Board", messages: messages });
+});
+
+/* POST new page. */
+router.post("/new", function (req, res, next) {
+  const newMsg = req.body;
+  messages.push({
+    text: newMsg.text,
+    user: newMsg.author,
+    added: dateFns.format(new Date(), "dd/MMM/y '-' hh:mm:ss"),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
